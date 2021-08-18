1d9879cac737a9d7ea6efdc469e6052e64cfde25
SaleDjerfi <69083655+SaleDjerfi@users.noreply.github.com>
2021-08-18 18:37:07 +0200

    ejs file

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
-const stripeSecretKey = process.env.STRIPE_SECRET_KEY
-const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
-
-const express = require('express')
-const app = express()
-
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+    require('dotenv').config()
+  }
+  
+  const stripeSecretKey = process.env.STRIPE_SECRET_KEY
+  const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
+  
+  const express = require('express')
+  const app = express()
+  const fs = require('fs')
+  const stripe = require('stripe')(stripeSecretKey)
+  
+  app.set('view engine', 'ejs')
+  app.use(express.json())
+  app.use(express.static('public'))
+  
+  app.get('/store', function(req, res) {
+    fs.readFile('items.json', function(error, data) {
+      if (error) {
+        res.status(500).end()
+      } else {
+        res.render('store.ejs', {
+          stripePublicKey: stripePublicKey,
+          items: JSON.parse(data)
+        })
+      }
+    })
+  })
 
 app.listen(3000)
